cdd9cb18b58e6bb4cee8197ddfed83088dc9e3c2
wuzhengbin <wzbdroid@gmail.com>
2016-11-30 15:12:03 +0800

    First

---
+++ WZBMQTTManagerFramework.framework/Versions/A/Headers/WZBMQTTManager.h
@@ -0,0 +1,13 @@
+//
+//  WZBMQTTManager.h
+//  Pods
+//
+//  Created by wuzhengbin on 2016/11/30.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WZBMQTTManager : NSObject
+- (void)testMethod;
+@end
